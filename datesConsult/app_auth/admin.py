c4d96890f797017f2108295ece25a8c0712ccdcf
Ramona Gospodinova <ramona.gospodinova@plana.solutions>
2023-03-21 17:44:13 +0200

    Basic Django init project with Postgresql

--- datesConsult/app_auth/admin.py
+++ datesConsult/app_auth/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from datesConsult.forms.app_auth_forms import *
+
+UserModel = get_user_model()
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserUpdateForm
+    model = CustomUser
+    list_display = ("email", "is_staff", )
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "groups", "user_permissions")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "email", "password1", "password2", "is_staff", "groups", "user_permissions"
+            )}
+        ),
+    )
+    search_fields = ("email",)
+    ordering = ("email",)
--- datesConsult/settings.py
+++ datesConsult/settings.py
@@ -57,7 +57,7 @@ WSGI_APPLICATION = 'datesConsult.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'dates_consult',
+        'NAME': 'dates_consult2',
         'USER': 'postgres',
         'PASSWORD': '1123QwER',
         'HOST': 'localhost',
